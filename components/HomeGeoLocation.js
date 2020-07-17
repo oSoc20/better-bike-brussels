9f1b8951a92bd606b104b80dda26e3dcf25e4907
Wout.Verbiest <wout.verbiest@student.howest.be>
2020-07-17 13:53:37 +0200

    fetch data from server

--- components/HomeGeoLocation.js
+++ components/HomeGeoLocation.js
@@ -16,24 +16,71 @@ class HomeGeoLocation extends React.Component {
     if (!navigator.geolocation) {
       console.log("geolocation not available");
       this.setState({
-        displayComponents: true,
+        displayComponents: false,
         lat: 50.8503,
         lng: 4.33517,
       });
+      fetchLocationData().then(res => this.setState({
+        displayComponents: true,
+        lat: this.state.lat,
+        lng: this.state.lng,
+        data: res,
+    }));
     } else {
-      navigator.geolocation.getCurrentPosition((pos) => {
-        this.setState({
-          displayComponents: true,
-          lat: pos.coords.latitude,
-          lng: pos.coords.longitude,
-        });
-      }, ()=>{
-        this.setState({
-            displayComponents: true,
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          this.setState({
+            displayComponents: false,
+            lat: pos.coords.latitude,
+            lng: pos.coords.longitude,
+          });
+          fetchLocationData().then(res => this.setState({
+              displayComponents: true,
+              lat: this.state.lat,
+              lng: this.state.lng,
+              data: res,
+          }));
+        },
+        () => {
+          this.setState({
+            displayComponents: false,
             lat: 50.8503,
             lng: 4.33517,
           });
+          fetchLocationData().then(res => this.setState({
+            displayComponents: true,
+            lat: this.state.lat,
+            lng: this.state.lng,
+            data: res,
+        }));
+        }
+      );
+    }
+
+    async function fetchLocationData() {
+      let host = "http://localhost:8080";
+
+      const endpoints = (await getData(`${host}/api/v1/map/endpoints`)).success;
+
+      const promises = endpoints.map((endpoint) =>
+        getData(
+          `${host}${endpoint}?lat=50.849747&lng=4.3511706&radius=2000&max_answers=5`
+        )
+      );
+
+      let data = await Promise.all(promises).then((responses) => {
+        return responses;
       });
+      console.log(data);
+      return data;
+    }
+
+    function getData(url) {
+      return fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          return data;
+        });
     }
   }
 
@@ -41,22 +88,32 @@ class HomeGeoLocation extends React.Component {
     let displayComponents = this.state.displayComponents;
     let latitude = this.state.lat;
     let longitude = this.state.lng;
+    let data = this.state.data;
 
-    return <div>
-        
+    return (
+      <div>
         <h2>What do you want to find?</h2>
+        <p>list view</p>
 
-        <h2>Within a <i>2 km</i> radius from you</h2>
-        
-        {displayComponents ? <div>{latitude}, {longitude}</div> : ""}
-        
+        <h2>
+          Within a <i>2 km</i> radius from you
+        </h2>
+
+        {displayComponents ? (
+          <div>
+            {latitude}, {longitude}, {data[0].features[0].id}
+          </div>
+        ) : (
+          "loading"
+        )}
 
         <style jsx>{`
-          i{
-              background-color:yellow;
+          i {
+            background-color: yellow;
           }
         `}</style>
-        </div>;
+      </div>
+    );
   }
 }
 export default HomeGeoLocation;
