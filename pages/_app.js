9ea556c91dbb78e8c86ffcc70caad03eb97394fc
Wout.Verbiest <wout.verbiest@student.howest.be>
2020-07-18 19:26:24 +0200

    add general stylesheet

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../public/stylesheets/reset.css'
+import '../public/stylesheets/style.css'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
